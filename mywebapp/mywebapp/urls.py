00402ed546ef994b653a2c42e3b9d048e1812da1
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2021-08-30 00:37:46 -0400

    Part 8: User Profile and Picture

--- mywebapp/mywebapp/urls.py
+++ mywebapp/mywebapp/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 from users import views as user_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +30,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- mywebapp/users/apps.py
+++ mywebapp/users/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    # from django documentation,
+    # see django -> topics -> signals -> "Where should this code live?"
+    def ready(self):
+        import users.signals

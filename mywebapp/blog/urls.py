70dbcde9f751f787d7e5a23ba6c6129d7a32436d
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2021-08-31 15:39:19 -0400

    Part 10: Create, Update and Delete Posts

--- mywebapp/blog/urls.py
+++ mywebapp/blog/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home-page"),
+    path('', views.PostListView.as_view(), name="home-page"),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', views.PostCreateView.as_view(), name="post-create"),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name="post-edit"),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name="blog-about")
 ]
